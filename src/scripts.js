416286f474eabc98dd97d4f76c35fe8a9f235efe
Juan Vitor <juandolino8@gmail.com>
2023-12-16 16:48:48 -0300

    bugs fixed and added some animation.

--- src/scripts.js
+++ src/scripts.js
@@ -6,8 +6,9 @@ let operationObject = {
 };
 
 const displayNumber = document.querySelector(".displayNumber");
+
 const previousValue = document.querySelector(".previousValue");
-const currentOperation = document.querySelector(".currentOperation");
+const currentOperator = document.querySelector(".currentOperator");
 
 // That function set the element "displayNumber"
 const setDisplayNumber = (clickedNumber) => {
@@ -17,7 +18,8 @@ const setDisplayNumber = (clickedNumber) => {
     operationObject.secondNumber += clickedNumber;
   }
   displayNumber.innerText =
-    (operationObject.firstNumber == operationObject.result && operationObject.secondNumber == "") || operationObject.operator == null
+    (operationObject.firstNumber == operationObject.result && operationObject.secondNumber == "" && operationObject.operator == null) ||
+    operationObject.operator == null
       ? operationObject.firstNumber
       : operationObject.secondNumber;
 };
@@ -25,8 +27,16 @@ const setDisplayNumber = (clickedNumber) => {
 const setOperator = (clickedOperator) => {
   doOperation();
   operationObject.operator = operationObject.firstNumber != "" ? clickedOperator : null;
+  currentOperator.innerText = operationObject.operator;
 
   if (operationObject.firstNumber != "" && operationObject.secondNumber == "") setDisplayNumber("");
+
+  previousValue.innerText = operationObject.firstNumber;
+  previousValue.style.animation = "none";
+
+  setTimeout(() => {
+    previousValue.style.animation = "previousValueAnimation 0.2s forwards";
+  }, 20);
 };
 
 const doOperation = () => {
@@ -53,6 +63,13 @@ const doOperation = () => {
 };
 
 const setResult = () => {
+  previousValue.style.animation = "none";
+  currentOperator.innerText = "";
+
+  setTimeout(() => {
+    previousValue.style.animation = "previousValueAnimation 0.2s forwards reverse";
+  }, 2);
+
   operationObject = {
     firstNumber: operationObject.result,
     secondNumber: "",
@@ -64,15 +81,12 @@ const setResult = () => {
 
 // That function delete the last number on display, its a backspace.
 const backspaceDisplayNumber = () => {
-  if (
-    operationObject.firstNumber == operationObject.result ||
-    (operationObject.firstNumber != "" && operationObject.secondNumber == "" && operationObject.operator == null)
-  ) {
-    operationObject.firstNumber = operationObject.firstNumber.substring(0, operationObject.firstNumber.length - 1);
-    displayNumber.innerText = operationObject.firstNumber;
-  } else if (operationObject.firstNumber != "" && operationObject.operator != null) {
+  if (operationObject.firstNumber != "" && operationObject.operator != null) {
     operationObject.secondNumber = operationObject.secondNumber.substring(0, operationObject.secondNumber.length - 1);
     displayNumber.innerText = operationObject.secondNumber;
+  } else {
+    operationObject.firstNumber = operationObject.firstNumber.substring(0, operationObject.firstNumber.length - 1);
+    displayNumber.innerText = operationObject.firstNumber;
   }
 };
 
@@ -85,4 +99,9 @@ const clearAll = () => {
   };
 
   displayNumber.innerText = 0;
+  currentOperator.innerText = operationObject.operator;
+  previousValue.style.animation = "none";
+  setTimeout(() => {
+    previousValue.style.animation = "previousValueAnimation 0.2s forwards reverse";
+  }, 2);
 };
