22afb6825945cf9393ad63d6437acda9f8a81b97
Juan Vitor <juandolino8@gmail.com>
2023-12-16 14:19:55 -0300

    added function to backspace button and AC button.

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,8 @@ let operationObject = {
 };
 
 const displayNumber = document.querySelector(".displayNumber");
+const previousValue = document.querySelector(".previousValue");
+const currentOperation = document.querySelector(".currentOperation");
 
 // That function set the element "displayNumber"
 const setDisplayNumber = (clickedNumber) => {
@@ -20,12 +22,9 @@ const setDisplayNumber = (clickedNumber) => {
       : operationObject.secondNumber;
 };
 
-// That function delete the last number on display, its a backspace.
-const backspaceDisplayNumber = () => {};
-
 const setOperator = (clickedOperator) => {
   doOperation();
-  operationObject.operator = clickedOperator;
+  operationObject.operator = operationObject.firstNumber != "" ? clickedOperator : null;
 
   if (operationObject.firstNumber != "" && operationObject.secondNumber == "") setDisplayNumber("");
 };
@@ -61,4 +60,29 @@ const setResult = () => {
     result: operationObject.result,
   };
   setDisplayNumber("");
+};
+
+// That function delete the last number on display, its a backspace.
+const backspaceDisplayNumber = () => {
+  if (
+    operationObject.firstNumber == operationObject.result ||
+    (operationObject.firstNumber != "" && operationObject.secondNumber == "" && operationObject.operator == null)
+  ) {
+    operationObject.firstNumber = operationObject.firstNumber.substring(0, operationObject.firstNumber.length - 1);
+    displayNumber.innerText = operationObject.firstNumber;
+  } else if (operationObject.firstNumber != "" && operationObject.operator != null) {
+    operationObject.secondNumber = operationObject.secondNumber.substring(0, operationObject.secondNumber.length - 1);
+    displayNumber.innerText = operationObject.secondNumber;
+  }
+};
+
+const clearAll = () => {
+  operationObject = {
+    firstNumber: "",
+    secondNumber: "",
+    operator: null,
+    result: null,
+  };
+
+  displayNumber.innerText = 0;
 };
